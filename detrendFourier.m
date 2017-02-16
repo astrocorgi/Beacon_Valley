00d28d6367dd755a16ec5cac05f3ce2756076b23
Cassie Stuurman <cassiestuurman@gmail.com>
2017-02-16 15:49:57 -0600

    Topographic analysis for beacon valley

--- detrendFourier.m
+++ detrendFourier.m
@@ -4,22 +4,38 @@ function [ output_args ] = detrendFourier( elevation_file )
 %covered glaciers. Expected input is an n x 2 x,y elevation profile.
 %   Detailed explanation goes here
 
+
+    set(0,'defaultAxesFontSize',16);
     input = load(elevation_file);
     x = input(:,1); %metres
-    y = input(:,2); %metres
+    elev = input(:,2); %metres
     figure(1);
+    subplot(3,1,1);
     plot(x,elev);
+    hold on
     xlabel('Distance, km');
     ylabel('Elevation, m');
     title('Input Elevation Profile');
     
-    %Fs = 1; %1 m resolution, which I think is the spatial sampling 'frequency'
-    %T = 1/Fs;
-    %L=length(elev);
-    %t = (0:L-1)*T;        % Inverse x-axis
-    elev_fft = fft(elev);
-    figure(2)
-    plot(elev_fft);
-    
+    detrend_elev = detrend(elev);
+    subplot(3,1,2);
+    plot(x,detrend_elev);
+    hold on
+    xlabel('Distance, km');
+    ylabel('Elevation, m');
+    title('Detrended Elevation Profile');
+   
+    Fs = 1; %1 m resolution, which I think is the spatial sampling 'frequency'
+    T = 1/Fs;
+    L=length(detrend_elev);
+    n = 2^nextpow2(L); %for padding the input with trailing zeroes, for fft performance
+    elev_fft = fft(detrend_elev,n);
+    inv_x = Fs*(0:(n/2))/n;
+    P2 = abs(elev_fft/L); %two-sided spectrum
+    subplot(3,1,3);
+    loglog(inv_x,P2(1:n/2+1));
+    title('Frequency spectrum for the detrended elevation profile');
+    xlabel('Spatial frequency (1/m)');
+    ylabel('Amplitude');
 end
 
